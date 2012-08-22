27eee8292de0568690e88e440f1499f285332ccb
Chris Tracey <tracey.chris@gmail.com>
2012-08-22 15:18:26 +1000

    initialize object with splunk server details. use search instead of execute. use HTTP/NET instead of curl. (pair:ggiesemann)

--- lib/splunkman/search.rb
+++ lib/splunkman/search.rb
@@ -1,14 +1,14 @@
 module SplunkMan
-  class Search
+  class Search 
 
-    def initialize(splunk_server, user, password, splunk_search)
-      @splunk_search = splunk_search
+    def initialize(splunk_server, user, password)
       @splunk_server = splunk_server
       @user = user
       @password = password
     end
 
-    def execute
+    def search(search_query)
+      @splunk_search = search_query 
       search_id = initiate_splunk_search @splunk_search
       wait_for_splunk_search(search_id)
       get_splunk_search_result search_id
@@ -18,8 +18,21 @@ module SplunkMan
 
     def initiate_splunk_search(splunk_search)
       filter_command = "egrep -o '<sid>.*</sid>' | sed -e 's/<sid>//;s/<\\/sid>//"
-      splunk_search_request = "curl -k -s -u #{@user}:#{@password} #{@splunk_server}/search/jobs -d #{splunk_search} | #{filter_command} '"
-      `#{splunk_search_request}`.chomp
+      uri = URI("#{@splunk_server}/search/jobs")
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request.basic_auth(@user, @password)
+      request.form_data = { 'search' => splunk_search }
+
+      response = http.request(request)
+      search_id = nil
+      if match = response.body.match(%r{<sid>([0-9\.]+)</sid>})
+        search_id = match[1]
+      end
+      search_id
     end
 
     def wait_for_splunk_search(search_id)
@@ -28,6 +41,7 @@ module SplunkMan
       done = ''
       while done.empty? do
         filter_command = "grep -e isDone..1"
+        puts "curl -k -s -u #{@user}:#{@password} #{@splunk_server}/search/jobs/#{search_id} | #{filter_command}"
         done=`curl -k -s -u #{@user}:#{@password} #{@splunk_server}/search/jobs/#{search_id} | #{filter_command}` 
         print "."
       end
@@ -35,7 +49,8 @@ module SplunkMan
 
     def get_splunk_search_result(search_id)
       splunk_search_result = "curl -k -s -u #{@user}:#{@password} #{@splunk_server}/search/jobs/#{search_id}/results --get -d output_mode=json"
-      JSON.parse(`#{splunk_search_result}`)
+      `#{splunk_search_result}`
     end
   end
 end
+
